5d3c031f255187410d25d03776b887728fa7d4de
Yanhao Li <1310724226@qq.com>
2023-06-14 00:11:04 +0800

    SGL、SimGCL
    SGL、SimGCL

---
+++ README.md
@@ -0,0 +1,17 @@
+<h1>推荐系统框架 pytorch版</h1>
+
+<h2>Requirements</h2>
+	
+```
+numba==0.53.1
+numpy==1.20.3
+scipy==1.6.2
+tensorflow==1.14.0
+torch>=1.7.0
+```
+
+<h2>Model</h2>
+- SGL
+- SimGCL
+- HeavyGCL (myself)
+- LightGCL (myself)
---
+++ main.py
@@ -0,0 +1,31 @@
+from SELFRec import SELFRec
+from util.conf import ModelConf
+import time
+
+if __name__ == '__main__':
+    # Register your model here
+    graph_baselines = ['LightGCN']
+    ssl_graph_models = ['SGL', 'SimGCL', 'HeavyGCL', 'LightGCL']
+
+    print('=' * 80)
+    print('   SELFRec: A library for self-supervised recommendation.   ')
+    print('=' * 80)
+
+    print('Graph-Based Baseline Models:')
+    print('   '.join(graph_baselines))
+    print('-' * 100)
+    print('Self-Supervised  Graph-Based Models:')
+    print('   '.join(ssl_graph_models))
+    print('=' * 80)
+    model = input('Please enter the model you want to run:')
+
+    s = time.time()
+    if model in graph_baselines or model in ssl_graph_models:
+        conf = ModelConf('./conf/' + model + '.conf')
+    else:
+        print('Wrong model name!')
+        exit(-1)
+    rec = SELFRec(conf)
+    rec.execute()
+    e = time.time()
+    print("Running time: %f s" % (e - s))
